82ce6eca6696d98968e517f77857db44ea735ad9
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-08-04 22:39:42 +0530

    Create Employee.cs

---
+++ Models/Employee.cs
@@ -0,0 +1,35 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Employee Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Employee Code")]
+        [Required]
+        public string Code { get; set; }
+
+        [DisplayName("Date Od Birth ")]
+        [Required]
+        public DateOnly DOB { get; set; }
+
+        [DisplayName("City")]
+        [Required]
+        public string City { get; set; }
+
+        [DisplayName("Gender")]
+        [Required]
+        public string Gender { get; set; }
+
+        [DisplayName("Status")]
+        [Required]
+        public string Status { get; set; }
+
+    }
+}
